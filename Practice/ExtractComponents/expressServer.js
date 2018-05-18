8f1bbf95c4309b750a5dbb8fe151bbdf874f5108
Christian Walker <chrsitw@g.clemson.edu>
2018-05-18 12:19:49 -0400

    Delete expressServer.js

--- Practice/ExtractComponents/expressServer.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const serve   = require('express-static');
-const app = express();
-app.use(serve(__dirname + '/'));
-app.listen(3000);
-console.log("Listening on port 3000...");
